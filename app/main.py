0f151ea31831b9e61e882aa49e0704977ada5c88
Takada Yuki <crane_271828@outlook.jp>
2017-10-31 20:14:33 +0900

    エラー処理

--- app/main.py
+++ app/main.py
@@ -1,9 +1,21 @@
+import math
 def main(argv):
+    if len(argv) !=3:
+        return -1
+    if not(argv[0].isdigit() and argv[1].isdigit() and argv[2].isdigit()):
+        return -1
+    a = int(argv[0])
+    b = int(argv[1])
+    m = int(argv[2])
+    if math.gcd(a,b) != 1:
+        return -1
+
   # このコードは引数と標準出力を用いたサンプルコードです。
   # このコードは好きなように編集・削除してもらって構いません。
   # ---
   # This is a sample code to use arguments and outputs.
   # Edit and remove this code as you like.
 
-  for i, v in enumerate(argv):
-    print("argv[{0}]: {1}".format(i, v))
+# if __name__ == '__main__':
+#     input_array = ["2","3","1"]
+#     main(input_array)
