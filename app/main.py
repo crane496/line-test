7c7fed887e93a0b2979bc37ce20c10fabfdd4e5a
Takada Yuki <crane_271828@outlook.jp>
2017-10-31 20:16:22 +0900

    print文に変更

--- app/main.py
+++ app/main.py
@@ -1,14 +1,17 @@
 import math
 def main(argv):
     if len(argv) !=3:
-        return -1
+        print("-1")
+        exit()
     if not(argv[0].isdigit() and argv[1].isdigit() and argv[2].isdigit()):
-        return -1
+        print("-1")
+        exit()
     a = int(argv[0])
     b = int(argv[1])
     m = int(argv[2])
     if math.gcd(a,b) != 1:
-        return -1
+        print("-1")
+        exit()
 
   # このコードは引数と標準出力を用いたサンプルコードです。
   # このコードは好きなように編集・削除してもらって構いません。
